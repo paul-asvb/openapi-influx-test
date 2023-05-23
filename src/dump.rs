602e4f21a5f1a9fc473d1201d53939a7d52aa1ce
Paul Mayer <paul@paul-mayer.eu>
2023-05-23 17:49:03 +0200

    init s3

---
+++ src/dump.rs
@@ -0,0 +1,54 @@
+use axum::{
+    routing::{get, post},
+    Router, BoxError,
+};
+use dotenvy::dotenv;
+use reqwest::StatusCode;
+
+use axum::{
+    body::Bytes,
+};
+use futures::{Stream, TryStreamExt};
+use std::io;
+use tokio::{fs::File, io::BufWriter};
+use tokio_util::io::StreamReader;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+/// Register a Device
+#[utoipa::path(
+    post,
+    path = "/bin-dump",
+    request_body = DeviceRegister,
+    responses(
+        (status = 200, description = "Data successfully dumped"),
+        (status = 500, description = "Error while dumping")
+    )
+)]
+// Save a `Stream` to a file
+async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, String)>
+where
+    S: Stream<Item = Result<Bytes, E>>,
+    E: Into<BoxError>,
+{
+    if !path_is_valid(path) {
+        return Err((StatusCode::BAD_REQUEST, "Invalid path".to_owned()));
+    }
+
+    async {
+        // Convert the stream into an `AsyncRead`.
+        let body_with_io_error = stream.map_err(|err| io::Error::new(io::ErrorKind::Other, err));
+        let body_reader = StreamReader::new(body_with_io_error);
+        futures::pin_mut!(body_reader);
+
+        // Create the file. `File` implements `AsyncWrite`.
+        let path = std::path::Path::new(UPLOADS_DIRECTORY).join(path);
+        let mut file = BufWriter::new(File::create(path).await?);
+
+        // Copy the body into the file.
+        tokio::io::copy(&mut body_reader, &mut file).await?;
+
+        Ok::<_, io::Error>(())
+    }
+    .await
+    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
+}
